1af23a462057f1004d73d90d4fde718cb6779d20
Awambeng <114798938+Awambeng@users.noreply.github.com>
2025-08-26 10:07:50 +0100

    Centralize OID4VCI Protocol Constants in Oid4VciConstants and Refactor Usages (#111)
    Closes #40083
    
    Signed-off-by: forkimenjeckayang <forkimenjeckayang@gmail.com>
    Co-authored-by: forkimenjeckayang <forkimenjeckayang@gmail.com>
    Co-authored-by: forkimenjeckayang <104195313+forkimenjeckayang@users.noreply.github.com>

--- services/src/main/java/org/keycloak/protocol/oid4vc/issuance/OID4VCIssuerEndpoint.java
+++ services/src/main/java/org/keycloak/protocol/oid4vc/issuance/OID4VCIssuerEndpoint.java
@@ -124,8 +124,8 @@ public class OID4VCIssuerEndpoint {
     public static final String NONCE_PATH = "nonce";
     public static final String CREDENTIAL_PATH = "credential";
     public static final String CREDENTIAL_OFFER_PATH = "credential-offer/";
-    public static final String RESPONSE_TYPE_IMG_PNG = "image/png";
-    public static final String CREDENTIAL_OFFER_URI_CODE_SCOPE = "credential-offer";
+    public static final String RESPONSE_TYPE_IMG_PNG = Oid4VciConstants.RESPONSE_TYPE_IMG_PNG;
+    public static final String CREDENTIAL_OFFER_URI_CODE_SCOPE = Oid4VciConstants.CREDENTIAL_OFFER_URI_CODE_SCOPE;
     private final KeycloakSession session;
     private final AppAuthManager.BearerTokenAuthenticator bearerTokenAuthenticator;
     private final TimeProvider timeProvider;
