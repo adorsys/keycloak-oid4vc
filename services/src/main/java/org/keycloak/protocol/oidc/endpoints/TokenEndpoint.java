85711f0d72bf7be02ef6abe3d49e4467ad6e0413
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-09-11 14:09:11 +0100

    update: revert changes in TokenEndpoint

--- services/src/main/java/org/keycloak/protocol/oidc/endpoints/TokenEndpoint.java
+++ services/src/main/java/org/keycloak/protocol/oidc/endpoints/TokenEndpoint.java
@@ -146,7 +146,6 @@ public class TokenEndpoint {
 
         OAuth2GrantType.Context context = new OAuth2GrantType.Context(session, clientConfig, clientAuthAttributes,
                                                                       formParams, event, cors, tokenManager);
-        context.setProtocol(new OIDCLoginProtocol(session, realm, session.getContext().getUri(), headers, event));
         return grant.process(context);
     }
 
